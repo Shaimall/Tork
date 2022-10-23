f510ed7f3a032b0ec74f42719587d4b43c023428
Shaimall <115071814+Shaimall@users.noreply.github.com>
2022-10-23 03:56:17 -0700

    Add files via upload

---
+++ components/P9.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const P9 = () => {
+  return (
+    <div className="bg-[url('/img/5.jpg')] bg-cover h-[1037px]  ">
+      <div className="bg-[#835FA8] w-[760px] h-[692px]">
+        <div className="container pt-32 ">
+          <p className="text-white leading-relaxed text-4xl w-[600px] pl-44">
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+            Nisi ut aliquip ex ea commodo.
+          </p>
+          <div className="bg-white w-[56px] h-[56px] pl-12"> </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default P9;
